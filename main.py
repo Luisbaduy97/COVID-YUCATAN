af5b4870f89c9d0b5e769bff120861e8f3abe055
Luis Navarrete-Baduy <baduy97@users.noreply.github.com>
2020-04-24 03:31:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -171,8 +171,8 @@ app.layout = html.Div([html.Div(children = [html.H1('Casos acumulados en Yucatá
 
 
 # local
-if __name__ == '__main__':
-     app.run_server(debug=True)
+#if __name__ == '__main__':
+#     app.run_server(debug=True)
 
 # gunicorn
-#app = app.server
+app = app.server
